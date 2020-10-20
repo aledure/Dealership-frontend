1a87a4409b75ec3d291c4e019e1407cf77386411
aledure <mandrew.ledure@gmail.com>
2020-10-19 19:10:06 -0500

    added local server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var sorceSsl = require('force-ssl-heroku');
+const path = require('path');
+const enableProdMode = require('@angular/core').enableProdMode
+
+enableProdMode();
+
+const app = express();
+
+app.use(forceSsl);
+
+app.use(express.static('./dist/dealership-frontend'));
+
+app.get('/*', (req,res)=>{
+  res.sendFile(path.join(__dirname,'./dist/dealership-frontend/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('Server started')
+})
